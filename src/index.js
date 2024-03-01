4cf40b808949f50ed05ce009207b0aa13b9465a0
leesantosyt <sites.externosff@gmail.com>
2024-03-01 20:12:56 -0300

    fifth-commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import RoutesApp from './routes.js';
 import reportWebVitals from './reportWebVitals';
 import RoutesApp from './routes.js';
 import { HashRouter } from "react-router-dom";
@@ -12,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <RoutesApp/>
     </HashRouter>
   </React.StrictMode>
 );
