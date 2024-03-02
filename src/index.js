428430b9b6dbb2f41283b7af67938f3ecbfc58d0
leesantosyt <sites.externosff@gmail.com>
2024-03-01 21:53:15 -0300

    cores-letras

--- src/index.js
+++ src/index.js
@@ -3,17 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './routes.js';
-import { HashRouter } from "react-router-dom";
 
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App/>
-    </HashRouter>
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
